ba1f32474f7c00d7dfedb9d4107fbc9133fc1990
njb2163 <njb2163@columbia.edu>
2025-02-09 22:01:01 -0500

    cleanup

--- nick-website/src/App.js
+++ nick-website/src/App.js
@@ -1,11 +1,8 @@
 import React from "react";
 import { HashRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./Home";
-import Projects from "./Projects";
-import Research from "./Research";
 import Header from "./Header";
 import Footer from "./Footer";
-import ProjectDetails from "./ProjectDetails";
 import './output.css';
 import './App.css';
 
@@ -15,9 +12,6 @@ function App() {
         <Header />
           <Routes>
             <Route path="/" element={<Home />} />
-            <Route path="/projects" element={<Projects />} />
-            <Route path="/research" element={<Research />} />
-            <Route path="/project-details" element={<ProjectDetails />} />
           </Routes>
         <Footer />
     </Router>
