fe760f09c18098270c9a5600cc565fbcc40849b6
njb2163 <njb2163@columbia.edu>
2025-02-10 19:35:12 -0500

    added experience

--- nick-website/src/Home.js
+++ nick-website/src/Home.js
@@ -8,51 +8,109 @@ import '@fortawesome/fontawesome-free/css/all.min.css';
 
 const aboutMe = "Hi, I'm Nicholas Bindela, a Software and Machine Learning Engineer passionate about building scalable solutions at the intersection of AI, cloud infrastructure, and product development. I have experience leading engineering teams, designing AWS-based architectures, and deploying machine learning models to production. Currently, I'm a Software Engineer II at Chewy, where I design and implement cloud-based ML pipelines for supply chain optimization. I'm also the Co-founder and Engineering Lead of a Stealth Startup, shaping the product vision and managing a team to deliver a functional MVP. Explore my projects below, and feel free to download my resume to learn more about my journey!"
 
-const mlData = [
+const projectsData = [
   {
-      image: FcAreaChart,
-      title: 'Commerical Credit Risk Modeling',
-      github_link: 'https://github.com/njb2163/Commercial-Credit-Risk',
-      description: 'Trained models to predict commercial loan defaults',
-      text_color: 'text-blue-500'
+    image: FcAreaChart,
+    title: 'Commerical Credit Risk Modeling',
+    github_link: 'https://github.com/njb2163/Commercial-Credit-Risk',
+    description: 'Trained models to predict commercial loan defaults',
+    text_color: 'text-blue-500',
+    tags: ["Machine Learning"],
+    start_date: "June 2024",
+    end_date: "August 2024"
   },
   {
-      image: FaSpotify,
-      title: 'Spotify Popularity Predictor',
-      github_link: 'https://github.com/njb2163/Spotify-Popularity-Predictor',
-      description: 'Trained models to predict commercial loan defaults',
-      text_color: 'text-[#1ED760]'
+    image: FaSpotify,
+    title: 'Spotify Popularity Predictor',
+    github_link: 'https://github.com/njb2163/Spotify-Popularity-Predictor',
+    description: 'Trained models to predict commercial loan defaults',
+    text_color: 'text-[#1ED760]',
+    tags: ["Machine Learning"],
+    start_date: "August 2024",
+    end_date: "December 2024"
   },
   {
-      image: TbBrandMinecraft,
-      title: 'Human Computational Models',
-      github_link: 'https://github.com/njb2163/Human-Computational-Models?tab=readme-ov-file',
-      description: 'Developed cognitive agents to simulate human behavior',
-      text_color: 'text-black'
+    image: TbBrandMinecraft,
+    title: 'Human Computational Models',
+    github_link: 'https://github.com/njb2163/Human-Computational-Models?tab=readme-ov-file',
+    description: 'Developed cognitive agents to simulate human behavior',
+    text_color: 'text-black',
+    tags: ["Artificial Intelligence"],
+    start_date: "June 2019",
+    end_date: "May 2020"
+  },
+  {
+    image: FcTodoList,
+    title: 'Todo List App',
+    github_link: 'https://github.com/njb2163/Todo-List',
+    description: 'Functional To-do list built using native Javascript, HTML, and CSS',
+    text_color: 'text-blue-500',
+    tags: ["Frontend"],
+    start_date: "August 2024",
+    end_date: "December 2024"
+  },
+  {
+    image: RiTreasureMapFill,
+    title: 'Quest Friender',
+    github_link: 'https://github.com/njb2163/Quest-Friender-App',
+    description: 'Fullstack Social Media React App',
+    text_color: 'text-[#C2B280]',
+    tags: ["Frontend", "Backend"],
+    start_date: "August 2024",
+    end_date: "December 2024"
+  },
+  {
+    image: FaShoppingBasket,
+    title: 'Mock Marketplace',
+    github_link: 'https://github.com/njb2163/Mock-Marketplace',
+    description: 'Mock marketplace webapp built using React, HTML, and CSS',
+    text_color: 'text-russian-violet-600',
+    tags: ["Frontend", "Backend"],
+    start_date: "August 2024",
+    end_date: "December 2024"
   },
 ];
 
-const sweData = [
+const experienceData = [
   {
-      image: FcTodoList,
-      title: 'Todo List App',
-      github_link: 'https://github.com/njb2163/Todo-List',
-      description: 'Functional To-do list built using native Javascript, HTML, and CSS',
-      text_color: 'text-blue-500'
+    image: "images/chewy-logo.png",
+    title: 'Chewy',
+    github_link: 'https://www.chewy.com/',
+    description: 'Software Engineer',
+    text_color: 'text-blue-500',
+    tags: ["AWS", "Backend"],
+    start_date: "September 2022",
+    end_date: "Present"
   },
   {
-      image: RiTreasureMapFill,
-      title: 'Quest Friender',
-      github_link: 'https://github.com/njb2163/Quest-Friender-App',
-      description: 'Fullstack Social Media React App',
-      text_color: 'text-[#C2B280]'
+    image: "images/ask2ai-logo.png",
+    title: 'Ask2AI',
+    github_link: 'https://www.ask2.ai/',
+    description: 'Machine Learning Research Assistant',
+    text_color: 'text-blue-200',
+    tags: ["Machine Learning"],
+    start_date: "August 2024",
+    end_date: "December 2024"
   },
   {
-      image: FaShoppingBasket,
-      title: 'Mock Marketplace',
-      github_link: 'https://github.com/njb2163/Mock-Marketplace',
-      description: 'Mock marketplace webapp built using React, HTML, and CSS',
-      text_color: 'text-russian-violet-600'
+    image: "images/fast-logo.svg",
+    title: 'FAST Enterprises',
+    github_link: 'https://www.fastenterprises.com/',
+    description: 'Implementation Consultant',
+    text_color: 'text-blue-200',
+    tags: ["Frontend", "Backend"],
+    start_date: "August 2021",
+    end_date: "September 2022"
+  },
+  {
+    image: "images/bucknell-logo.png",
+    title: 'Bucknell University',
+    github_link: 'https://www.bucknell.edu/',
+    description: 'AI Research Assistant',
+    text_color: 'text-blue-200',
+    tags: ["Artificial Intelligence"],
+    start_date: "June 2019",
+    end_date: "May 2020"
   },
 ];
 
@@ -63,26 +121,26 @@ const Home = () => {
   };
 
   return (
-    <div className="flex flex-col flex-grow text-gray-800">
-      <main className="flex flex-grow">
+    <div className="flex flex-col md:flex-row flex-grow text-gray-800">
+      <main className="flex flex-col md:flex-row flex-grow">
         {/* Left Column */}
-        <div className="w-1/3 p-6 flex flex-col items-center space-y-6 border-r border-gray-300">
+        <div className="w-full md:w-1/3 p-6 flex flex-col items-center space-y-6 border-b md:border-r border-gray-300">
           <img
             src="/Nickb.png"
             alt="Headshot"
-            className="w-full max-w-sm h-auto rounded-full shadow-lg border border-gray-300"
+            className="w-3/4 md:w-full max-w-sm h-auto rounded-full shadow-lg border border-gray-300"
           />
           <button
             onClick={handleResumeDownload}
-            className="text-white hover:text-blue-700 text-lg font-bold bg-russian-violet-600 border border-blue-500 w-full px-6 py-2 rounded-lg transition-transform transform hover:scale-105 text-center"
-            >
+            className="text-white hover:text-blue-700 text-sm md:text-lg font-bold bg-russian-violet-600 border border-blue-500 w-full px-6 py-3 rounded-lg transition-transform transform hover:scale-105 text-center"
+          >
             <i className="fa-regular fa-file-pdf"></i> Resume
           </button>
           <a
             href="https://www.linkedin.com/in/nicholasbindela/"
             target="_blank"
             rel="noopener noreferrer"
-            className="text-white hover:text-blue-700 text-lg font-bold bg-blue-500 border border-blue-500 w-full px-6 py-2 rounded-lg transition-transform transform hover:scale-105 text-center"
+            className="text-white hover:text-blue-700 text-sm md:text-lg font-bold bg-blue-500 border border-blue-500 w-full px-6 py-3 rounded-lg transition-transform transform hover:scale-105 text-center"
           >
             <i className="fab fa-linkedin mr-2"></i>LinkedIn
           </a>
@@ -90,30 +148,29 @@ const Home = () => {
             href="https://github.com/njb2163"
             target="_blank"
             rel="noopener noreferrer"
-            className="text-white hover:text-gray-600 text-lg font-bold w-full px-6 py-2 bg-[#2dba4e] rounded-lg transition-transform transform hover:scale-105 text-center"
+            className="text-white hover:text-gray-600 text-sm md:text-lg font-bold w-full px-6 py-3 bg-[#2dba4e] rounded-lg transition-transform transform hover:scale-105 text-center"
           >
             <i className="fab fa-github mr-2"></i>GitHub
           </a>
         </div>
 
         {/* Right Column */}
-        <div className="w-2/3 p-6 mx-auto text-center">
+        <div className="w-full md:w-2/3 p-6 mx-auto text-center">
           <div className="text-center">
             <h2 className="text-2xl font-bold text-gray-800">About Me</h2>
             <hr className="w-3/4 mt-2 border-t-2 border-gray-300 mx-auto" />
-            <p className="w-3/4 mt-4 text-lg mx-auto text-left">
+            <p className="w-full md:w-3/4 mt-4 text-sm md:text-lg mx-auto text-left">
               {aboutMe}
             </p>
           </div>
-          <div className="flex flex-grow space-x-6">
-            <div className="w-1/2">
-              <Research inputData={sweData} title={"Software Engineering"} showSeeAll={false} />
+          <div className="flex flex-col md:flex-row flex-grow space-y-6 md:space-x-6">
+            <div className="w-full md:w-1/2">
+              <Research inputData={projectsData} title={"Projects"} showSeeAll={false} />
             </div>
-            <div className="w-1/2">
-              <Research inputData={mlData} title={"Machine Learning and AI"} showSeeAll={false} />
+            <div className="w-full md:w-1/2">
+              <Research inputData={experienceData} title={"Experience"} showSeeAll={false} />
             </div>
           </div>
-
         </div>
       </main>
     </div>
--- nick-website/src/Research.js
+++ nick-website/src/Research.js
@@ -1,22 +1,34 @@
 import React from 'react';
-import { useNavigate } from 'react-router-dom';
 
 
 const Research = ({ inputData, title, showSeeAll }) => {
-    const navigate = useNavigate();
+    const tagColors = {
+        "Machine Learning": "bg-blue-500",
+        "Artificial Intelligence": "bg-green-500",
+        "Frontend": "bg-pink-500",
+        "Backend": "bg-red-500",
+        "AWS": "bg-yellow-500"
+    };
+
+    const sortedInputData = [...inputData].sort((a, b) => {
+        if (a.end_date === "Present") return -1;
+        if (b.end_date === "Present") return 1;
+      
+        return  new Date(b.end_date) - new Date(a.end_date);
+      });
 
     return (
-        <div className="w-full max-w-med h-auto">
-            <div className="mt-12 px-6">
-                <h2 className="text-2xl font-bold text-gray-800">{title}</h2>
-                <hr className="w-3/4 mt-2 border-t-2 border-gray-300 mx-auto" />
+        <div className="w-full max-w-full md:max-w-med h-auto">
+            <div className="mt-6 md:mt-12 px-4 md:px-6">
+                <h2 className="text-xl md:text-2xl font-bold text-gray-800">{title}</h2>
+                <hr className="w-full md:w-3/4 mt-2 border-t-2 border-gray-300 mx-auto" />
             </div>
 
-            <div className="mt-8 space-y-6 overflow-y-auto h-128">
-                {inputData.map((research, index) => (
+            <div className="mt-4 md:mt-8 space-y-4 md:space-y-6 overflow-y-auto h-auto md:h-128">
+                {sortedInputData.map((research, index) => (
                     <div
                         key={index}
-                        className="relative mx-auto w-3/4 p-6 bg-blue-100 text-gray-800 rounded-lg shadow-lg transition-all hover:scale-105"
+                        className="relative mx-auto w-full md:w-3/4 p-4 md:p-6 bg-blue-100 text-gray-800 rounded-lg shadow-lg transition-all hover:scale-105"
                     >
                         <a
                             href={research.github_link}
@@ -25,29 +37,25 @@ const Research = ({ inputData, title, showSeeAll }) => {
                             className={`${research.text_color}`}
                         >
                             <div className="flex flex-col items-center gap-4 sm:flex-row sm:items-center">
-                                <research.image
-                                size={70}
-                                />
+                                {typeof research.image === 'string' ? (
+                                    <img src={research.image} alt={research.title} className='w-12 h-12 md:w-16 md:h-16' />
+                                ) : (
+                                    <research.image size={window.innerWidth < 768 ? 50 : 70} />
+                                )}
                                 <div className="text-center sm:text-left">
-                                    <h2 className="text-lg font-semibold text-gray-800">{research.title}</h2>
-                                    <p className="text-gray-600">{research.description}</p>
-
+                                    <h2 className="text-md md:text-lg font-semibold text-gray-800">{research.title}</h2>
+                                    <p className="text-sm text-gray-600">{research.description}</p>
+                                    <p className="text-xs text-gray-600 mt-1">{research.start_date} - {research.end_date}</p>
+                                    <div className="flex flex-wrap gap-2 mt-2">
+                                        {research.tags && research.tags.map(tag => (
+                                            <span key={tag} className={`px-3 py-1 text-xs font-semibold text-white rounded-full ${tagColors[tag] || 'bg-gray-500'}`}>{tag}</span>
+                                        ))}
+                                    </div>
                                 </div>
                             </div>
                         </a>
                     </div>
                 ))}
-                {showSeeAll && (
-                    <div
-                        className="relative mx-auto w-3/4 p-6 bg-blue-100 text-gray-800 rounded-lg shadow-lg transition-all hover:scale-110"
-                        onClick={() => navigate('/projects')}
-                    >
-                        <div className="flex justify-between text-center sm:text-left text-2xl">
-                            <h2 className="text-lg font-semibold text-gray-800">See all research</h2>
-                            <i class="fa-solid fa-arrow-right"></i>
-                        </div>
-                    </div>
-                )}
             </div>
         </div>
     );
